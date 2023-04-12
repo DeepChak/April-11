b72bf75d8f9ca21a86ad04b53657fac5b6891929
DeepChak <127760974+DeepChak@users.noreply.github.com>
2023-04-12 11:17:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    let res=document.getElementById("Firstname").value;
+    console.log(res);
+    let res1=document.getElementById("Middlename").value;
+    console.log(res1);
+    let res2=document.getElementById("Lastname").value;
+    console.log(res2);
+    let res3=document.getElementById("mail").value;
+    console.log(res3);
+    let res4=document.getElementById("Address").value;
+    console.log(res4);
+}
